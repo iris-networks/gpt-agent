2e3f2afac99f143bfbe8835e7a1d0ff443671a0b
codebanesr <shanur.cse.nitap@gmail.com>
2025-04-02 13:10:42 +0530

    feat: Removed the popup

--- extension/src/content-script.ts
+++ extension/src/content-script.ts
@@ -1,8 +1,5 @@
-
-// Main content script for the Zenobia agent
-import { initUI } from './modules/ui';
 import { initMessageListeners } from './modules/messaging';
 
-initUI();
+
 initMessageListeners();
 
