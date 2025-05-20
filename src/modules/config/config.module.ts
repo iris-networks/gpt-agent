d8d922320b1c4af63fd4d6b539892feba2fce9c9
codebanesr <shanur.cse.nitap@gmail.com>
2025-05-20 14:09:02 +0800

    feat: making some deployment changes

--- src/modules/config/config.module.ts
+++ src/modules/config/config.module.ts
@@ -5,11 +5,9 @@
 
 import { Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
-import { ConfigController } from './config.controller';
 
 @Module({
   providers: [ConfigService],
-  controllers: [ConfigController],
   exports: [ConfigService],
 })
 export class ConfigModule {}
